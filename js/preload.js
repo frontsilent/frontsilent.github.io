63ef355c04a8a5cece401560045aeaee775e5e11
frontsilent <silent0172@gmail.com>
2018-04-04 13:22:26 +0300

    add preload img

---
+++ js/preload.js
@@ -0,0 +1,9 @@
+var img = new Array();
+img[0] = new Image();
+img[1] = new Image();
+img[2] = new Image();
+
+
+img[0].src = "img/logo-mob-hover.svg"
+img[1].src = "img/logo-tablet-hover.svg"
+img[2].src = "img/logo-desc-hover.svg"

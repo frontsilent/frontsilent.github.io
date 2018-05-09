e90fc182b730a25b61c1491a4da9e79a3c4d43fa
frontsilent <silent0172@gmail.com>
2018-05-09 14:49:28 +0300

    first changes

--- js/custom.js
+++ js/custom.js
@@ -1,83 +1,97 @@
-$('.review__items').slick({
-	mobileFirst: true,
-	slidesToShow: 1,
-	speed: 300,
-	cssEase: "ease-in",
-	dots: true,
-	arrows: false,
-	adaptiveHeight: true,
-	responsive: [
-		{
-			breakpoint: 1199,
-			settings: {
-				dots: false,
-				arrows: true
-			}
+  $(function () {
+
+    $(".main-nav__button").click(function () {
+
+      $(".main-nav__items").slideToggle("slow");
+      $(".main-nav__toogle").toggleClass("main-nav__toogle--opened");
+      $(".main-nav__button").toggleClass("main-nav__button--opened");
+
+    });
+
+
+  });
+
+
+  $('.review__items').slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    speed: 300,
+    cssEase: "ease-in",
+    dots: true,
+    arrows: false,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          dots: false,
+          arrows: true
+        }
     }
   ]
 
-});
-$('.tariffs__slider').slick({
-	mobileFirst: true,
-	slidesToShow: 1,
-	speed: 300,
-	cssEase: "ease-in",
-	dots: true,
-	arrows: false,
-	initialSlide: 1,
-	responsive: [
-		{
-			breakpoint: 700,
-			settings: "unslick"
+  });
+  $('.tariffs__slider').slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    speed: 300,
+    cssEase: "ease-in",
+    dots: true,
+    arrows: false,
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 700,
+        settings: "unslick"
     }
   ]
 
-});
+  });
 
-function initMap() {
-	var coords = {
-		lat: 59.9391,
-		lng: 30.3232
-	};
-	var markerImage = "img/contacts-marker.svg"
-	var myMap = new google.maps.Map(document.querySelector(".map"), {
-		zoom: 17,
-		scrollwheel: false,
-		center: coords
-	});
+  function initMap() {
+    var coords = {
+      lat: 59.9391,
+      lng: 30.3232
+    };
+    var markerImage = "img/contacts-marker.svg"
+    var myMap = new google.maps.Map(document.querySelector(".map"), {
+      zoom: 17,
+      scrollwheel: false,
+      center: coords
+    });
 
-	var markers = [
-		{
-			coordinates: {
-				lat: 59.93895,
-				lng: 30.32329
-			},
-			image: markerImage,
-			info: "ул. Большая Конюшенная,19/8"
+    var markers = [
+      {
+        coordinates: {
+          lat: 59.93895,
+          lng: 30.32329
+        },
+        image: markerImage,
+        info: "ул. Большая Конюшенная,19/8"
 		}
 	]
 
-	for (var i = 0; i < markers.length; i++) {
-		addMarker(markers[i]);
-	}
+    for (var i = 0; i < markers.length; i++) {
+      addMarker(markers[i]);
+    }
 
-	function addMarker(properties) {
-		var marker = new google.maps.Marker({
-			position: properties.coordinates,
-			map: myMap
-		});
-		if (properties.image) {
-			marker.setIcon(properties.image);
-		}
-		if (properties.info) {
-			var infoWindow = new google.maps.InfoWindow({
-				content: properties.info
-			})
+    function addMarker(properties) {
+      var marker = new google.maps.Marker({
+        position: properties.coordinates,
+        map: myMap
+      });
+      if (properties.image) {
+        marker.setIcon(properties.image);
+      }
+      if (properties.info) {
+        var infoWindow = new google.maps.InfoWindow({
+          content: properties.info
+        })
 
-			marker.addListener("click", function () {
-				infoWindow.open(myMap, marker);
-			})
-			infoWindow.open(myMap, marker);
-		}
-	}
-}
+        marker.addListener("click", function () {
+          infoWindow.open(myMap, marker);
+        })
+        infoWindow.open(myMap, marker);
+      }
+    }
+  }
--- js/preload.js
+++ js/preload.js
@@ -2,6 +2,8 @@ var img = new Array();
 img[0] = new Image();
 img[1] = new Image();
 img[2] = new Image();
+img[3] = new Image();
+img[4] = new Image();
 
 
 
@@ -9,3 +11,5 @@ img[2] = new Image();
 img[0].src = "img/logo-mob-hover.svg"
 img[1].src = "img/logo-tablet-hover.svg"
 img[2].src = "img/logo-desc-hover.svg"
+img[3].src = "img/icons/open-btn-hover.svg"
+img[4].src = "img/icons/close-btn-hover.svg"
